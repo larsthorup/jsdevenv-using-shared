9d2079d8eef4dacc57eb895d3d919fd9c00326f8
Lars Thorup <lars@zealake.com>
2014-06-18 15:17:50 -0700

    jshint

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+/*global module, require*/
+module.exports = function (grunt) {
+    'use strict';
+
+    var gruntConfig = require('jsdevenv-shared/gruntConfig.js')(grunt);
+
+    grunt.initConfig(gruntConfig);
+};
